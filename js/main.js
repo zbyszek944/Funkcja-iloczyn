9a4602e579cd2a72b5741877af4d12e5151b25fd
zbyszek944 <zsyga1@dmail.com>
2023-12-18 10:15:45 +0100

    Funkcja-iloczyn

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// ES5
+// function iloczyn(num1, num2, num3) {
+//    let result = num1 * num2 * num3;
+
+//    return result;
+// }
+
+// let wynikIloczynu = iloczyn(2,3,4);
+
+// console.log (wynikIloczynu);
+
+
+// ES6
+
+
+// const iloczyn = (num1, num2, num3) => {
+//    let result = num1 * num2 * num3;
+
+//    return result;
+// }
+
+// let wynikIloczynu = iloczyn(2,3,4);
+// console.log(wynikIloczynu);
+
+// ES6 WERSJA SKRÓCONA
+
+const iloczyn = (num1, num2, num3) => num1 * num2 * num3;
+   
+
+
+
+let wynikIloczynu = iloczyn(2,3,4);
+console.log(wynikIloczynu);
